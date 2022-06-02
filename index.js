a907e38d434581fb1b23f27e8811adac8fa01aa5
maycao <caiobrone77@gmail.com>
2022-06-02 15:17:12 -0300

    criada a funcao que vai converter para link

--- index.js
+++ index.js
@@ -12,12 +12,21 @@ const manipulingText = (line, condition, newValue) => {
     fs.appendFileSync(contentPath, `${newValue}${output}${newValue.replace('<', '</')}\n`)
 }
 
+const manipulingLink = (line) => {
+    const infoLink = line.slice(7).replace(/].*/, '').split(',')
+    const contentLink = line.slice(7).replace(/.*]/, '').trim()
+    fs.appendFileSync(contentPath, `<a href=${infoLink[0]} target="${infoLink[1].trim()}">${contentLink}</a>`)
+}
+
 rl.on('line', (line) => {
     if (line.startsWith('[h1]')) {
         manipulingText(line, '[h1]', '<h1>');
     }
     if (line.startsWith('[h2]')) {
         manipulingText(line, '[h2]', '<h2>');
+    }
+    if (line.startsWith('[link]')) {
+        manipulingLink(line)
     }
     console.log(line);
 });
